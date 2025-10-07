ed59efb39616098367a82f248f05e28a271afc09
Frido <fridoavan24@gmail.com>
2025-10-07 11:46:54 +0700

    ubah input.tsx

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -2,13 +2,16 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  placeholderText?: string; // optional property supaya interface nggak kosong
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type, placeholderText, ...props }, ref) => {
     return (
       <input
         type={type}
+        placeholder={placeholderText}
         className={cn(
           "flex h-10 w-full rounded-md border border-gray-300 bg-white px-3 py-2 text-sm " +
             "placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50",
@@ -20,6 +23,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
     );
   }
 );
+
 Input.displayName = "Input";
 
 export { Input };
